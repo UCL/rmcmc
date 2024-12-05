89e150589f7af79e9789259fbcf8f230b35e0f5c
Matt Graham <matthew.m.graham@gmail.com>
2024-12-05 10:55:25 +0000

    Generalizing Hamiltonian proposal
    Allowing randomization of number of leapfrog
    step and partial momentum refreshment

--- R/hamiltonian.R
+++ R/hamiltonian.R
@@ -6,8 +6,13 @@ sample_hamiltonian <- function(
     target_distribution,
     n_step,
     scale_and_shape,
-    sample_auxiliary = stats::rnorm) {
-  state$update(momentum = sample_auxiliary(state$dimension()))
+    sample_auxiliary,
+    sample_n_step) {
+  momentum <- sample_auxiliary(state$momentum())
+  state$update(momentum = momentum)
+  if (!is.null(sample_n_step)) {
+    n_step <- sample_n_step(n_step)
+  }
   involution_hamiltonian(state, n_step, scale_and_shape, target_distribution)
 }
 
@@ -64,7 +69,21 @@ log_density_ratio_hamiltonian <- function(
 #' @inherit barker_proposal return params description
 #'
 #' @param n_step Number of leapfrog steps to simulate Hamiltonian dynamics for
-#'   in each proposed move.
+#'   in each proposed move, or parameter passed to function specified by
+#'   `sample_n_step` argument if not `NULL`.
+#' @param sample_auxiliary A function which samples new values for auxiliary
+#'   variables (corresponding to a linear transform of momentum) given previous
+#'   values, leaving their standard normal target distribution invariant.
+#'   Defaults to a function sampling independent standard normal random variates
+#'   but can be used to implement alternative updates such as partial momentum
+#'   refreshment.
+#' @param sample_n_step Optionally a function which randomly samples number of
+#'   leapfrog steps to simulate in each proposed move from some integer-valued
+#'   distribution, or `NULL` (the default) to use a fixed deterministic number
+#'   of steps as specified by `n_step` argument. If a function it should accept
+#'   a single argument which will be passed the value of `n_step` which can
+#'   be used to specify parameter(s) of distribution to sample number of steps
+#'   from.
 #'
 #' @export
 #'
@@ -73,21 +92,51 @@ log_density_ratio_hamiltonian <- function(
 #'   log_density = function(x) -sum(x^2) / 2,
 #'   gradient_log_density = function(x) -x
 #' )
+#'
+#' # Proposal with fixed number of leapfrog steps
 #' proposal <- hamiltonian_proposal(target_distribution, scale = 1., n_step = 5)
 #' state <- chain_state(c(0., 0.))
 #' withr::with_seed(876287L, proposed_state <- proposal$sample(state))
 #' log_density_ratio <- proposal$log_density_ratio(state, proposed_state)
 #' proposal$update(scale = 0.5)
+#'
+#' # Proposal with number of steps randomly sampled uniformly from 5:10
+#' proposal <- hamiltonian_proposal(
+#'   target_distribution,
+#'   scale = 1.,
+#'   n_step = c(5, 10),
+#'   sample_n_step = function(n) n[1] + sample.int(n[2] - n[1] + 1) - 1
+#' )
+#' withr::with_seed(876287L, proposed_state <- proposal$sample(state))
+#'
+#' # Proposal with partial momentum refreshment
+#' phi <- pi / 4
+#' proposal <- hamiltonian_proposal(
+#'   target_distribution,
+#'   scale = 1.,
+#'   n_step = 1,
+#'   sample_auxiliary = function(m) cos(phi) * m + sin(phi) * rnorm(length(m))
+#' )
+#' withr::with_seed(876287L, {
+#'   state <- chain_state(rnorm(2), rnorm(2))
+#'   proposed_state <- proposal$sample(state)
+#' })
 hamiltonian_proposal <- function(
     target_distribution,
     n_step,
     scale = NULL,
     shape = NULL,
-    sample_auxiliary = stats::rnorm) {
+    sample_auxiliary = function(auxiliary) stats::rnorm(length(auxiliary)),
+    sample_n_step = NULL) {
   scale_and_shape_proposal(
     sample = function(state, scale_and_shape) {
       sample_hamiltonian(
-        state, target_distribution, n_step, scale_and_shape, sample_auxiliary
+        state,
+        target_distribution,
+        n_step,
+        scale_and_shape,
+        sample_auxiliary,
+        sample_n_step
       )
     },
     log_density_ratio = function(state, proposed_state, scale_and_shape) {
