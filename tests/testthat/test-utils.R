d3801fc0e1fff6d9d5eb30c9fefb4f3cc6a0bdff
Matt Graham <matthew.m.graham@gmail.com>
2024-12-09 09:14:06 +0000

    Add tests for op

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -16,3 +16,24 @@ test_that("min_1_exp works", {
   expect_equal(min_1_exp(-1.), exp(-1))
   expect_equal(min_1_exp(1), 1)
 })
+
+test_that("is_non_scalar_vector works", {
+  expect_true(is_non_scalar_vector(c(1, 2)))
+  expect_false(is_non_scalar_vector(matrix(1:4, 2, 2)))
+  expect_false(is_non_scalar_vector(3))
+})
+
+test_that("matrix vector multiply %@% op works", {
+  m <- matrix(1:9, nrow = 3, ncol = 3)
+  v <- c(0.5, 2, -1.2)
+  s <- 0.8
+  expect_equal(drop(m %*% v), m %@% v)
+  expect_equal(drop(v %*% m), v %@% m)
+  expect_equal(drop(diag(v) %*% v), v %@% v)
+  expect_equal(s * v, s %@% v)
+  expect_equal(s * v, v %@% s)
+  expect_equal(s * s, s %@% s)
+  expect_error(m %@% s, regexp = "at least one vector")
+  expect_error(s %@% m, regexp = "at least one vector")
+  expect_error(m %@% m, regexp = "at least one vector")
+})
