03ca87fa7c7824e3f7fe16c22545b99ebe907416
Matt Graham <matthew.m.graham@gmail.com>
2024-12-10 17:01:20 +0000

    Remove cross reference to ramcmc chol_update

--- R/adaptation.R
+++ R/adaptation.R
@@ -291,7 +291,7 @@ variance_shape_adapter <- function(kappa = 1) {
 #' restatement of method proposed in Haario et al. (2001).
 #'
 #' Requires `ramcmc` package to be installed for access to efficient rank-1
-#' Cholesky update function [ramcmc::chol_update()].
+#' Cholesky update function `ramcmc::chol_update`.
 #'
 #' @param kappa Decay rate exponent in `[0.5, 1]` for adaptation learning rate.
 #'  Value of 1 (default) corresponds to computing empirical covariance matrix.
