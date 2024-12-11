1ec12aacf6ca735260fc47c771b4b55df5fc6a29
Matt Graham <matthew.m.graham@gmail.com>
2024-12-11 18:18:02 +0000

    Combine BridgeStan interface functions for constructing target distribution and trace function

--- R/bridges.R
+++ R/bridges.R
@@ -1,6 +1,13 @@
 #' Construct target distribution from a BridgeStan `StanModel` object.
 #'
 #' @param model Stan model object to use for target (posterior) distribution.
+#' @param include_log_density Whether to include an entry `log_density`
+#'   corresponding to current log density for target distribution in values
+#'   returned by trace function.
+#' @param include_generated_quantities Whether to included generated quantities
+#'   in Stan model definition in values returned by trace function.
+#' @param include_transformed_parameters Whether to include transformed
+#'   parameters in Stan model definition in values returned by trace function.
 #'
 #' @return A list with entries
 #' * `log_density`: A function to evaluate log density function for target
@@ -8,6 +15,9 @@
 #' * `value_and_gradient_log_density`: A function to evaluate value and gradient
 #'   of log density function for target distribution given current position
 #'   vector, returning as a list with entries `value` and `gradient`.
+#' * `trace_function`: A function which given a `chain_state()` object returns a
+#'   named vector of values to trace during sampling. The constrained parameter
+#'   values of model will always be included.
 #'
 #' @export
 #'
@@ -18,46 +28,13 @@
 #'   876287L, state <- chain_state(stats::rnorm(model$param_unc_num()))
 #' )
 #' state$log_density(target_distribution)
-target_distribution_from_stan_model <- function(model) {
-  list(
-    log_density = model$log_density,
-    value_and_gradient_log_density = function(position) {
-      value_and_gradient <- model$log_density_gradient(position)
-      names(value_and_gradient) <- c("value", "gradient")
-      value_and_gradient
-    }
-  )
-}
-
-#' Construct trace function from a BridgeStan `StanModel` object.
-#'
-#' @param model Stan model object to use to generate (constrained) parameters to
-#'   trace.
-#' @param include_log_density Whether to include an entry `log_density`
-#'   corresponding to current log density for target distribution in values
-#'   returned by trace function.
-#' @param include_generated_quantities Whether to included generated quantities
-#'   in Stan model definition in values returned by trace function.
-#' @param include_transformed_parameters Whether to include transformed
-#'   parameters in Stan model definition in values returned by trace function.
-#'
-#' @return A function which given `chain_state` object returns a named vector of
-#'   values to trace during sampling. The constrained parameter values of model
-#'   will always be included.
-#'
-#' @export
-#'
-#' @examplesIf requireNamespace("bridgestan", quietly = TRUE)
-#' model <- example_gaussian_stan_model()
-#' trace_function <- trace_function_from_stan_model(model)
-#' withr::with_seed(876287L, state <- chain_state(rnorm(model$param_unc_num())))
-#' trace_function(state)
-trace_function_from_stan_model <- function(
+#' target_distribution$trace_function(state)
+target_distribution_from_stan_model <- function(
     model,
     include_log_density = TRUE,
     include_generated_quantities = FALSE,
     include_transformed_parameters = FALSE) {
-  function(state) {
+  trace_function <- function(state) {
     position <- state$position()
     trace_values <- model$param_constrain(
       position, include_transformed_parameters, include_generated_quantities
@@ -68,15 +45,25 @@ trace_function_from_stan_model <- function(
     }
     trace_values
   }
+  list(
+    log_density = model$log_density,
+    value_and_gradient_log_density = function(position) {
+      value_and_gradient <- model$log_density_gradient(position)
+      names(value_and_gradient) <- c("value", "gradient")
+      value_and_gradient
+    },
+    trace_function = trace_function
+  )
 }
 
 #' Construct an example BridgeStan `StanModel` object for a Gaussian model.
 #'
 #' Requires BridgeStan package to be installed. Generative model is assumed to
-#' be of the form `y ~ normal(mu, sigma)` for unknown `mu` and `sigma`.
+#' be of the form `y ~ normal(mu, sigma)` for unknown `mu ~ normal(0, 3)` and
+#' `sigma ~ half_normal(0, 3)`.
 #'
 #' @param n_data Number of independent data points `y` to generate and condition
-#'   model against.
+#'   model against from `normal(0, 1)`.
 #' @param seed Integer seed for Stan model.
 #'
 #' @return BridgeStan StanModel object.
@@ -88,20 +75,22 @@ trace_function_from_stan_model <- function(
 #' model$param_names()
 example_gaussian_stan_model <- function(n_data = 50, seed = 1234L) {
   rlang::check_installed("bridgestan", reason = "to use this function")
-  model_string <- "data {
-  int<lower=0> N;
-  vector[N] y;
+  model_string <- "
+  data {
+    int<lower=0> N;
+    vector[N] y;
   }
   parameters {
     real mu;
     real<lower=0> sigma;
   }
   model {
+    mu ~ normal(0, 3);
+    sigma ~ normal(0, 3);
     y ~ normal(mu, sigma);
   }"
   withr::with_seed(seed, y <- stats::rnorm(n_data))
   data_string <- sprintf('{"N": %i, "y": [%s]}', n_data, toString(y))
-  model_file <- tempfile("gaussian", fileext = ".stan")
   withr::with_tempfile("model_file",
     {
       writeLines(model_string, model_file)
--- tests/testthat/test-bridges.R
+++ tests/testthat/test-bridges.R
@@ -25,10 +25,11 @@ test_that("Creating target distribution from Stan model works", {
   expect_type(target_distribution, "list")
   expect_named(
     target_distribution,
-    c("log_density", "value_and_gradient_log_density")
+    c("log_density", "value_and_gradient_log_density", "trace_function")
   )
   expect_type(target_distribution$log_density, "closure")
   expect_type(target_distribution$value_and_gradient_log_density, "closure")
+  expect_type(target_distribution$trace_function, "closure")
   position <- rep(0, model$param_unc_num())
   log_density <- target_distribution$log_density(position)
   value_and_gradient_log_density <- (
@@ -46,37 +47,43 @@ test_that("Creating target distribution from Stan model works", {
 })
 
 for (include_log_density in c(TRUE, FALSE)) {
-  test_that("Creating trace function from Stan model works", {
-    model <- cached_example_gaussian_stan_model()
-    trace_function <- trace_function_from_stan_model(
-      model,
-      include_log_density = include_log_density
-    )
-    expect_type(trace_function, "closure")
-    position <- rep(0, model$param_unc_num())
-    state <- chain_state(position)
-    trace_values <- trace_function(state)
-    if (include_log_density) {
-      expected_length <- model$param_num() + 1L
-    } else {
-      expected_length <- model$param_num()
+  test_that(
+    sprintf(
+      "Creating trace function from Stan model works (include_log_density = %i)",
+      include_log_density
+    ),
+    {
+      model <- cached_example_gaussian_stan_model()
+      trace_function <- target_distribution_from_stan_model(
+        model,
+        include_log_density = include_log_density
+      )$trace_function
+      expect_type(trace_function, "closure")
+      position <- rep(0, model$param_unc_num())
+      state <- chain_state(position)
+      trace_values <- trace_function(state)
+      if (include_log_density) {
+        expected_length <- model$param_num() + 1L
+      } else {
+        expected_length <- model$param_num()
+      }
+      expect_identical(length(trace_values), expected_length)
+      expected_parameter_values <- model$param_constrain(position)
+      expected_parameter_names <- model$param_names()
+      if (include_log_density) {
+        expected_names <- c(expected_parameter_names, "log_density")
+      } else {
+        expected_names <- expected_parameter_names
+      }
+      expect_named(trace_values, expected_names)
+      for (i in seq_along(expected_parameter_values)) {
+        name <- expected_parameter_names[[i]]
+        expect_identical(trace_values[[name]], expected_parameter_values[[i]])
+      }
+      if (include_log_density) {
+        expected_log_density <- model$log_density(position)
+        expect_identical(trace_values[["log_density"]], expected_log_density)
+      }
     }
-    expect_identical(length(trace_values), expected_length)
-    expected_parameter_values <- model$param_constrain(position)
-    expected_parameter_names <- model$param_names()
-    if (include_log_density) {
-      expected_names <- c(expected_parameter_names, "log_density")
-    } else {
-      expected_names <- expected_parameter_names
-    }
-    expect_named(trace_values, expected_names)
-    for (i in seq_along(expected_parameter_values)) {
-      name <- expected_parameter_names[[i]]
-      expect_identical(trace_values[[name]], expected_parameter_values[[i]])
-    }
-    if (include_log_density) {
-      expected_log_density <- model$log_density(position)
-      expect_identical(trace_values[["log_density"]], expected_log_density)
-    }
-  })
+  )
 }
