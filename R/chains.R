3fe2a84d0c285710cfcdc8773e953dc42d0e4762
Matt Graham <matthew.m.graham@gmail.com>
2024-12-11 18:37:41 +0000

    Allow passing formula or Stan model directly to sample_chain

--- R/chains.R
+++ R/chains.R
@@ -4,21 +4,31 @@
 #' target distribution and proposal (defaulting to Barker proposal), optionally
 #' adapting proposal parameters in a warm-up stage.
 #'
-#' @param target_distribution Target stationary distribution for chain. A list
-#'   with named entries `log_density` and `gradient_log_density` corresponding
-#'   to respectively functions for evaluating the logarithm of the (potentially
-#'   unnormalized) density of the target distribution and its gradient (only
-#'   required for gradient-based proposals). As an alternative to
-#'   `gradient_log_density` an entry `value_and_gradient_log_density` may
-#'   instead be provided which is a function returning both the value and
-#'   gradient of the logarithm of the (unnormalized) density of the target
-#'   distribution as a list under the names `value` and `gradient` respectively.
-#'   The list may also contain a named entry `trace_function`, correspond to a
-#'   function which given current chain state outputs list of variables to trace
-#'   on each main (non-adaptive) chain iteration. If a `trace_function` entry is
-#'   not specified, then the default behaviour is to trace the position
-#'   component of the chain state along with the log density of the target
-#'   distribution.
+#' @param target_distribution Target stationary distribution for chain. One of:
+#'   * A one-sided formula specifying expression for log density of target
+#'     distribution which will be passed to
+#'     [target_distribution_from_log_density_formula()] to construct functions
+#'     to evaluate log density and its gradient using [deriv()].
+#'   * A `bridgestan::StanModel` instance (requires `bridgestan` to be
+#'     installed) specifying target model and data. Will be passed to
+#'     [target_distribution_from_stan_model()] using default values for optional
+#'     arguments - to override call [target_distribution_from_stan_model()]
+#'     directly and pass the returned list as the `target_distribution` argument
+#'     here.
+#'   * A list with named entries `log_density` and `gradient_log_density`
+#'     corresponding to respectively functions for evaluating the logarithm of
+#'     the (potentially unnormalized) density of the target distribution and its
+#'     gradient (only required for gradient-based proposals). As an alternative
+#'     to `gradient_log_density` an entry `value_and_gradient_log_density` may
+#'     instead be provided which is a function returning both the value and
+#'     gradient of the logarithm of the (unnormalized) density of the target
+#'     distribution as a list under the names `value` and `gradient`
+#'     respectively. The list may also contain a named entry `trace_function`,
+#'     correspond to a function which given current chain state outputs list of
+#'     variables to trace on each main (non-adaptive) chain iteration. If a
+#'     `trace_function` entry is not specified, then the default behaviour is to
+#'     trace the position component of the chain state along with the log
+#'     density of the target distribution.
 #' @param initial_state Initial chain state. Either a vector specifying just
 #'   the position component of the chain state or a list output by `chain_state`
 #'   specifying the full chain state.
@@ -103,7 +113,21 @@ sample_chain <- function(
   } else {
     stop("initial_state must be a vector or list with an entry named position.")
   }
-  if (is.null(target_distribuion$trace_function)) {
+  if (is(target_distribution, "formula")) {
+    target_distribution <- target_distribution_from_log_density_formula(
+      target_distribution
+    )
+  } else if (is(target_distribution, "StanModel")) {
+    target_distribution <- target_distribution_from_stan_model(
+      target_distribution
+    )
+  } else if (
+    !is.list(target_distribution) ||
+      !("log_density" %in% names(target_distribution))
+  ) {
+    stop("target_distribution invalid - see documentation for allowable types.")
+  }
+  if (is.null(target_distribution$trace_function)) {
     trace_function <- default_trace_function(target_distribution)
   } else {
     trace_function <- target_distribution$trace_function
