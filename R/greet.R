1ee1c8296dc5077c48d5a9a6cc41aa83ff585496
yuxinliu6996 <liuyuxin-20@outlook.com>
2026-06-08 15:22:49 +0100

    there is some new update

---
+++ R/greet.R
@@ -0,0 +1,13 @@
+#' Greet a person by name
+#'
+#' @param name: Name a person to greet
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' greet("Yuxin)
+greet <- function(name){
+  cat(paste0("Hello, ",name))
+}
+
